55c96d4ee9926f6b8096065ff7eb3b97b92d9efe
Kieu Anh (Katie) Pham <phamkieuanh33@gmail.com>
2023-06-26 00:19:53 +1000

    Update current date and time for default cities

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
+function updateTime() {
+  //London
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  londonDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = moment()
+    .tz("Europe/London")
+    .format("h:mm:ss:SSS [<small>]A[</small>]");
 
+  //Ho Chi Minh City
+  let hochiminhcityElement = document.querySelector("#hochiminh-city");
+  let hochiminhcityDateElement = hochiminhcityElement.querySelector(".date");
+  let hochiminhcityTimeElement = hochiminhcityElement.querySelector(".time");
+  hochiminhcityDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  hochiminhcityTimeElement.innerHTML = moment()
+    .tz("Asia/Ho_Chi_Minh")
+    .format("h:mm:ss:SSS [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1);
