667b6a84e0ee757cb40ae0038cfd5c303bfc10d2
Kieu Anh (Katie) Pham <phamkieuanh33@gmail.com>
2023-06-26 01:01:08 +1000

    Add functionality for select

--- src/index.js
+++ src/index.js
@@ -1,22 +1,46 @@
 function updateTime() {
   //London
   let londonElement = document.querySelector("#london");
-  let londonDateElement = londonElement.querySelector(".date");
-  let londonTimeElement = londonElement.querySelector(".time");
-  londonDateElement.innerHTML = moment().format("MMMM Do YYYY");
-  londonTimeElement.innerHTML = moment()
-    .tz("Europe/London")
-    .format("h:mm:ss:SSS [<small>]A[</small>]");
+  if (londonElement) {
+    let londonDateElement = londonElement.querySelector(".date");
+    let londonTimeElement = londonElement.querySelector(".time");
+    londonDateElement.innerHTML = moment().format("MMMM Do YYYY");
+    londonTimeElement.innerHTML = moment()
+      .tz("Europe/London")
+      .format("h:mm:ss:SSS [<small>]A[</small>]");
+  }
 
   //Ho Chi Minh City
   let hochiminhcityElement = document.querySelector("#hochiminh-city");
-  let hochiminhcityDateElement = hochiminhcityElement.querySelector(".date");
-  let hochiminhcityTimeElement = hochiminhcityElement.querySelector(".time");
-  hochiminhcityDateElement.innerHTML = moment().format("MMMM Do YYYY");
-  hochiminhcityTimeElement.innerHTML = moment()
-    .tz("Asia/Ho_Chi_Minh")
-    .format("h:mm:ss:SSS [<small>]A[</small>]");
+  if (hochiminhcityElement) {
+    let hochiminhcityDateElement = hochiminhcityElement.querySelector(".date");
+    let hochiminhcityTimeElement = hochiminhcityElement.querySelector(".time");
+    hochiminhcityDateElement.innerHTML = moment().format("MMMM Do YYYY");
+    hochiminhcityTimeElement.innerHTML = moment()
+      .tz("Asia/Ho_Chi_Minh")
+      .format("h:mm:ss:SSS [<small>]A[</small>]");
+  }
 }
 
 updateTime();
 setInterval(updateTime, 1);
+
+function updateCity(event) {
+  let cityTimezone = event.target.value;
+  let cityName = cityTimezone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimezone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format(
+      "h:mm:ss:SSS"
+    )} <small>${cityTime.format("A")}</small></div>
+    `;
+}
+
+let citiesSelectElement = document.querySelector("#select-city");
+citiesSelectElement.addEventListener("change", updateCity);
